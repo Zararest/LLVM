594d08d9cd554e4a1a2fff6675dd52210aea02ce
UwU <iishumakov@gmail.com>
2023-10-18 02:42:08 +0300

    New program

---
+++ New-waves/build/CMakeFiles/Waves.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/uwu/Proga/LLVM-course/New-waves/Engine.c" "/home/uwu/Proga/LLVM-course/New-waves/build/CMakeFiles/Waves.dir/Engine.c.o"
+  "/home/uwu/Proga/LLVM-course/New-waves/lib/Graphics.c" "/home/uwu/Proga/LLVM-course/New-waves/build/CMakeFiles/Waves.dir/lib/Graphics.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../SDL2Test"
+  "/usr/include/SDL2"
+  "../lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ New-waves/build/CMakeFiles/Waves.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Waves.dir/Engine.c.o"
+  "CMakeFiles/Waves.dir/lib/Graphics.c.o"
+  "Waves"
+  "Waves.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Waves.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
