9bd0d2be42b63134ce08544fb868f38584b9fa0b
UwU <iishumakov@gmail.com>
2023-10-25 18:08:00 +0300

    TMP

---
+++ Voronoi/Logger.cpp
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+void logFuncStart(uint64_t BBId) {
+
+}
--- Voronoi/Profiler-pass.cpp
+++ Voronoi/Profiler-pass.cpp
@@ -1,49 +1,126 @@
-#include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
-#include <cassert>
+#include <memory>
+#include <vector>
 
 using namespace llvm;
 
 namespace {
-  struct MyPass : public FunctionPass {
-    static char ID;
-    MyPass() : FunctionPass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-      //assert(false);
-      outs() << "In a function called " << F.getName() << "!\n";
+class Profiler : public FunctionPass {
+  std::unique_ptr<IRBuilder<>> Builder;
 
-      outs() << "Function body:\n";
-      F.print(outs());
+  auto &getOrCreateIRBuilder(LLVMContext &Ctx) {
+    if (Builder)
+      return *Builder;
+    Builder = std::make_unique<IRBuilder<>>(Ctx);
+    return *Builder;
+  }
 
-      for (auto &B : F) {
-        outs() << "Basic block:\n";
-        B.print(outs());
+  bool isLoggerFunction(StringRef Name) {
+    return Name == "logFuncStart";
+  }
 
-        for (auto &I : B) {
-          outs() << "Instruction: \n";
-          I.print(outs(), true);
-          outs() << "\n";
-        }
-      }
+  bool isEngineFunction(StringRef Name) {
+    return Name == "xorshift"  || Name == "changePosition" ||
+           Name == "updateDot" || Name == "changeState" ||
+           Name == "distance"  || Name == "getNearestDot" ||
+           Name == "drawFrame" || Name == "initDots" ||
+           Name == "app";
+  }
 
-      return false;
+  void generateBBPrologue(BasicBlock &BB) {
+  }
+
+  void generateBBEpilogue(BasicBlock &BB) {
+
+  }
+
+  void generateFuncPrologue(Function &F) {
+    auto &Ctx = F.getContext();
+    auto &IB = getOrCreateIRBuilder(Ctx);
+    auto ParamTypes = ArrayRef<Type *>{IB.getInt8Ty()->getPointerTo()};
+    auto RetType = Type::getVoidTy(Ctx);
+    
+    auto FuncType = FunctionType::get(RetType, ParamTypes, false);
+    auto Func = 
+      F.getParent()->getOrInsertFunction("logFuncStart", FuncType);
+    auto FuncName = IB.CreateGlobalStringPtr(F.getName());
+    auto Args = std::vector<Value *>{FuncName};
+    
+    IB.SetInsertPoint(&F.getEntryBlock().front());
+    IB.CreateCall(Func, Args);
+    outs() << "Inserted func prologue in " << F.getName() << "\n";
+  }
+
+  void generateFuncEpilogue(Function &F) {
+    
+  }
+
+  void generateBinOp(Instruction &I) {
+
+  }
+
+  void generateCall(Instruction &I) {
+    
+  }
+
+  void generateRet(Instruction &I) {
+    
+  }
+
+  void generateCmp(Instruction &I) {
+    
+  }
+
+  void runOnBB(BasicBlock &BB) {
+    generateBBPrologue(BB);
+    for (auto &I : BB) {
+      if (auto *BinOp = dyn_cast<BinaryOperator>(&I))
+        generateBinOp(I);
+      if (auto *Call = dyn_cast<CallInst>(&I))
+        generateCall(I);
+      if (auto *Ret = dyn_cast<ReturnInst>(&I)) 
+        generateRet(I);
+      if (auto *Cmp = dyn_cast<CmpInst>(&I))
+        generateCmp(I);
     }
-  };
-}
+    generateBBEpilogue(BB);
+  }
 
-char MyPass::ID = 0;
+public:
+  static char ID;
+  
+  Profiler() : FunctionPass(ID) {}
 
-// Automatically enable the pass.
-// http://adriansampson.net/blog/clangpass.html
-static void registerMyPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
-  PM.add(new MyPass());
+  virtual bool runOnFunction(Function &F) override {
+    if (isLoggerFunction(F.getName()) ||
+        !isEngineFunction(F.getName()))
+      return false;
+
+    generateFuncPrologue(F);
+    for (auto &BB : F)
+      runOnBB(BB);
+    generateFuncEpilogue(F);
+    return true;
+  }
+};
+} // namespace
+
+char Profiler::ID = 0;
+
+static void registerProfiler(const PassManagerBuilder &PMBuilder,
+                                 legacy::PassManagerBase &PM) {
+  PM.add(new Profiler());
 }
 static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerMyPass);
+  RegisterProfiler(PassManagerBuilder::EP_OptimizerLast,
+                   registerProfiler);
